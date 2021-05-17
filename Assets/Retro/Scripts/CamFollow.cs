2a0f2e8ac0030024dd9d042cdf6d711e1579afb8
김 현준 <jkajks123456@einssnc.com>
2021-05-17 09:11:47 +0900

    카메라 추적

---
+++ Assets/Retro/Scripts/CamFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public enum State {
+        Idle,
+        Ready,
+        Tracking,
+    }
+
+    private State state {
+        set {
+            switch (value) {
+                case State.Idle:
+                    break;
+                case State.Ready:
+                    break;
+                case State.Tracking:
+                    break;
+            }
+        }
+    }
+    private Transform target;
+
+    private void Awake() {
+        state = State.Idle;
+    }
+}
